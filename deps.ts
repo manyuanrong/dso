a336d6a31d287c0206002d3563cd3c2ff09d4922
EnokMan <416828041@qq.com>
2019-08-05 19:30:52 +0800

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
 export { assert, assertEquals, assertThrowsAsync } from "https://deno.land/std/testing/asserts.ts";
 export { runTests, test, TestFunction } from "https://deno.land/std/testing/mod.ts";
-export { Client, ClientConfig, Connection } from "https://deno.land/x/mysql@1.1.6/mod.ts";
+export { Client, ClientConfig, Connection } from "https://deno.land/x/mysql@1.2.0/mod.ts";
 export { Join, Order, Query, replaceParams, Where } from "https://deno.land/x/sql_builder@1.3.5/mod.ts";
 
 import "./src/Reflect.ts";
