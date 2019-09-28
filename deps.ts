5d7b6e17f2e398836222f40ee5fc8e089d6fcf4f
Enok <416828041@qq.com>
2019-09-28 12:04:50 +0800

    Bump deno to v0.19.0

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
-export { assert, assertEquals, assertThrowsAsync } from "https://deno.land/std@v0.17.0/testing/asserts.ts";
-export { runTests, test, TestFunction } from "https://deno.land/std@v0.17.0/testing/mod.ts";
-export { Client, ClientConfig, Connection } from "https://deno.land/x/mysql@1.2.2/mod.ts";
+export { assert, assertEquals, assertThrowsAsync } from "https://deno.land/std@v0.19.0/testing/asserts.ts";
+export { runTests, test, TestFunction } from "https://deno.land/std@v0.19.0/testing/mod.ts";
+export { Client, ClientConfig, Connection } from "https://deno.land/x/mysql@1.2.3/mod.ts";
 export { Join, Order, Query, replaceParams, Where } from "https://deno.land/x/sql_builder@1.3.5/mod.ts";
 
 import "./src/Reflect.ts";
